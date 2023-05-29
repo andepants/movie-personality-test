6b4ceff94aa224083b5033127f9b8a2d12d3b29d
dmwcats <dmwcats123@gmail.com>
2023-05-29 16:40:44 -0500

    skeleton api&movielist

--- src/app/results/page.js
+++ src/app/results/page.js
@@ -1,15 +1,28 @@
-import Link from 'next/link'
+import Link from "next/link";
+import Movie from "./Movie.jsx";
+
+const testData = {
+  movies: [
+    "The Shawshank Redemption",
+    "The Godfather",
+    "Pulp Fiction",
+    "The Dark Knight",
+    "Fight Club",
+  ],
+};
 
 export default function Results() {
   return (
     <main>
-      <h1 class="flex m-5 p-2 justify-center text-5xl font-bold text-gray-800 mb-4">This is the Result component</h1>
+      {testData.movies.map((movie) => (
+        <Movie movieTitle={movie} />
+      ))}
 
-      <Link href='/' className="flex justify-center">
+      <Link href="/" className="flex justify-center">
         <button className="px-4 m-2 py-2 rounded bg-blue-500 text-white font-bold">
           Home
         </button>
       </Link>
     </main>
-  )
+  );
 }
