dc714e0932a05081118ea39f6456d33abc40b218
Divya <dm.divya.mahajan@gmail.com>
2023-05-29 14:23:40 -0700

    recommendation

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,13 @@
+"use client"
+import React from 'react';
+import Recommendation from '../component/Recommendation';
+
 export default function Home() {
+  const searchQuery = 'Romantic Comadies';
   return (
     <main>
       <h1 class="flex m-5 p-2 justify-center text-5xl font-bold text-gray-800 mb-4">Movie Personality Test</h1>
+      <Recommendation searchQuery={searchQuery} />
     </main>
   )
 }
