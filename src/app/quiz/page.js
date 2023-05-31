64e8b185b38d15c43d4510b69ba4ecb908aee32c
Andrew Heim <andrewsheim@gmail.com>
2023-05-31 10:05:46 -0600

    adding new css

--- src/app/quiz/page.js
+++ src/app/quiz/page.js
@@ -1,24 +1,23 @@
-'use client';
-import Link from 'next/link'
-import { questions } from '../../../public/questions'
-import { useEffect, useState } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+"use client";
+import Link from "next/link";
+import { questions } from "../../../public/questions";
+import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Quiz() {
-
-  const [ currentQuestion, setCurrentQuestion ] = useState(0);
-  const [ selectedOptions, setSelectedOptions ] = useState([]);
-  const [ searchString, setSearchString ] = useState('');
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [selectedOptions, setSelectedOptions] = useState([]);
+  const [searchString, setSearchString] = useState("");
 
   const handleAnswerOption = (answer) => {
     setSelectedOptions([
       (selectedOptions[currentQuestion] = { userResponse: answer }),
     ]);
     setSelectedOptions([...selectedOptions]);
-    let search = '';
+    let search = "";
     for (let i = 0; i < selectedOptions.length; i++) {
-      search += selectedOptions[i].userResponse + ' ';
+      search += selectedOptions[i].userResponse + " ";
     }
     setSearchString(search);
   };
@@ -30,7 +29,7 @@ export default function Quiz() {
 
   const handleNext = () => {
     if (!selectedOptions[currentQuestion]?.userResponse) {
-      toast.error('Choose an Option!', {
+      toast.error("Choose an Option!", {
         position: "top-center",
         autoClose: 1000,
         hideProgressBar: false,
@@ -39,7 +38,7 @@ export default function Quiz() {
         draggable: true,
         progress: undefined,
         theme: "light",
-        });
+      });
       return;
     }
     const nextQues = currentQuestion + 1;
@@ -47,14 +46,14 @@ export default function Quiz() {
   };
 
   return (
-    <main className="flex flex-col w-screen px-5 h-screen bg-darkblue justify-center items-center">
-      <div className="bg-darkblue p-20 m-5 rounded-xl">
+    <main className="flex flex-col w-screen px-5 h-screen bg-gray-900 justify-center items-center">
+      <div className="bg-gray-900 p-20 m-5 rounded-xl">
         <div className="flex flex-col items-start w-full">
-          <h4 className="mt-10 text-xl text-lightblue">
+          <h4 className="mt-10 text-xl text-white">
             Question {currentQuestion + 1} of {questions.length}
           </h4>
-          <div className="mt-4 text-2xl text-offwhite">
-          {questions[currentQuestion].question}
+          <div className="mt-4 text-2xl text-white">
+            {questions[currentQuestion].question}
           </div>
         </div>
 
@@ -62,7 +61,7 @@ export default function Quiz() {
           {questions[currentQuestion].answerOptions.map((answer, index) => (
             <div
               key={index}
-              className="flex items-center w-full py-4 pl-5 m-2 ml-0 space-x-2 border-2 cursor-pointer bg-white/5 border-white/10 rounded-xl"
+              className="flex items-center w-full py-4 pl-5 m-2 ml-0 space-x-2 border-2 cursor-pointer bg-gray-800 border-white/10 rounded-xl"
               onClick={(e) => handleAnswerOption(answer.answer)}
             >
               <input
@@ -71,42 +70,45 @@ export default function Quiz() {
                 value={answer.answer}
                 onChange={(e) => handleAnswerOption(answer.answer)}
                 checked={
-                  answer.answer === selectedOptions[currentQuestion]?.userResponse
+                  answer.answer ===
+                  selectedOptions[currentQuestion]?.userResponse
                 }
                 className="w-6 h-6 bg-black"
               />
-              <p className="ml-6 text-offwhite">{answer.answer}</p>
+              <p className="ml-6 text-white">{answer.answer}</p>
             </div>
           ))}
         </div>
 
-        <div className="flex justify-between w-full mt-4 text-offwhite">
+        <div className="flex justify-between w-full mt-4 text-white">
           <button
             onClick={handlePrevious}
-            className="w-[49%] py-3 bg-pink rounded-lg"
+            className="w-[49%] py-3 bg-blue-500 rounded-lg"
           >
             Previous
           </button>
-          {currentQuestion + 1 === questions.length
-            ? <Link
-                href={{
-                  pathname: '/results',
-                  query: searchString,
-                }}
-                className="w-[49%] py-3 bg-pink rounded-lg text-center"
-              >
-                <button>Submit</button>
-              </Link>
-            : <button
-                onClick={handleNext}
-                className="w-[49%] py-3 bg-pink rounded-lg"
-              >
-                Next
-              </button>
-          }
+          {currentQuestion + 1 === questions.length ? (
+            <Link
+              href={{
+                pathname: "/results",
+                query: searchString,
+              }}
+              className="w-[49%] py-3 bg-blue-500 rounded-lg text-center"
+            >
+              <button>Submit</button>
+            </Link>
+          ) : (
+            <button
+              onClick={handleNext}
+              className="w-[49%] py-3 bg-blue-500 rounded-lg"
+            >
+              Next
+            </button>
+          )}
         </div>
       </div>
-      <Link href='/' className="flex justify-center">
+
+      <Link href="/" className="flex justify-center">
         <button className="px-4 m-2 py-2 rounded bg-blue-500 text-white font-bold">
           Home
         </button>
@@ -125,5 +127,5 @@ export default function Quiz() {
         theme="light"
       />
     </main>
-  )
+  );
 }
