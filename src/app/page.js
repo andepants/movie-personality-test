a0829cd1ccaa74699c5b75420e3555599df9e94a
Andrew Heim <andrewsheim@gmail.com>
2023-05-31 11:59:26 -0600

    css updates

--- src/app/page.js
+++ src/app/page.js
@@ -9,27 +9,22 @@ export default function Home() {
   const personalityDescriptionData =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vitae commodo tellus. Sed eu tristique elit. Vestibulum ac orci condimentum, ullamcorper risus eu, fermentum neque. Integer ullamcorper est nec ligula lobortis, in dictum massa aliquet. Nam volutpat ligula quis feugiat tempus. Quisque eleifend neque ac feugiat rutrum. Suspendisse potenti. Sed pharetra pellentesque risus non tempus. Mauris ut quammi. Aliquam eu purus vitae ex tincidunt rutrum at vel tellus. Prointempor nibh vel venenatis egestas. Curabitur dapibus augue at nislvolutpat, id finibus massa tristique. Suspendisse potenti. Donec sitamet diam ac quam feugiat congue ut non ex. Sed dictum tortor id enimhendrerit feugiat. Donec sit amet diam ac quam feugiat congue ut nonex. Sed dictum tortor id enim hendrerit feugiat.";
 
-  const handleClick = async () => {
-    let personalityData = await fetch('/api/personality' , {
-      method: 'POST',
-      body: JSON.stringify({ keywords : query})
-    })
-    personalityData = await personalityData.json();
-    console.log('personalityData: ', personalityData);
-  }
-
   return (
-    <main>
-      <h1 className="flex m-5 p-2 justify-center text-5xl font-bold text-gray-800 mb-4">
+    <main className="flex flex-col w-screen px-5 h-screen bg-gray-900 justify-center items-center">
+      <h1 className="flex m-5 p-2 justify-center text-8xl font-bold text-white mb-4">
         Movie Personality Test
       </h1>
 
+      <span className="flex m-2 p-2 justify-center text-xl text-white">
+        Receive Movie Recommendations Based on your Personality!
+      </span>
+
       <Link href="/quiz" className="flex justify-center">
         <button className="px-4 m-2 py-2 rounded bg-blue-500 text-white font-bold">
-          Quiz
+          Take the Quiz!
         </button>
       </Link>
-      <Link
+      {/* <Link
         href={{
           pathname: "/results",
           query: {
@@ -43,16 +38,8 @@ export default function Home() {
         <button className="px-4 m-2 py-2 rounded bg-blue-500 text-white font-bold">
           Results
         </button>
-      </Link>
-
+      </Link> */}
       {/* <Recommendation searchQuery={searchQuery} /> */}
-
-      <button
-        onClick={handleClick}
-        className="px-4 m-2 py-2 rounded bg-blue-500 text-white font-bold"
-      >
-          Send REQUEST to /personality
-      </button>
     </main>
   );
 }
