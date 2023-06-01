7928655f4dcf52469ce3a2d2d6b0ccaae45cecd2
Andrew Heim <andrewsheim@gmail.com>
2023-06-01 12:39:58 -0600

    inlcuding all questions with API

--- public/questions.js
+++ public/questions.js
@@ -56,357 +56,213 @@ export const questions = [
     }
     ]
     },
-  //   {
-  //   "question": "Do you often find yourself reading or learning about historical events, figures, or civilizations?",
-  //   "answerOptions": [
-  //   {
-  //   "answer": "Yes",
-  //   "keywords": "Historical Epics, Period, Documentaries, War, Archaeological"
-  //   },
-  //   {
-  //   "answer": "Occasionally",
-  //   "keywords": "Historical Epics, Period"
-  //   },
-  //   {
-  //   "answer": "Not really"
-  //   },
-  //   {
-  //   "answer": "Never"
-  //   }
-  //   ]
-  //   },
-  // {
-  //   "question": "Do you enjoy exploring the latest advancements in science and technology?",
-  //   "answerOptions": [
-  //     {
-  //       "answer": "Yes",
-  //       "keywords": "Science, Technology, Future, space, Robots, planet, universe, sci-fi"
-  //     },
-  //     {
-  //       "answer": "Occasionally",
-  //       "keywords": "Science, Technology, Robots, sci-fi"
-  //     },
-  //     {
-  //       "answer": "Not really"
-  //     },
-  //     {
-  //       "answer": "Never"
-  //     }
-  //   ]
-  // },
-  // {
-  //   "question": "Do you actively engage in discussions or activities for social causes and strive to make a positive impact in your community?",
-  //   "answerOptions": [
-  //     {
-  //       "answer": "Yes",
-  //       "keywords": "Social justice, Civil rights, discrimination, inequality, activism, empowerment, philosophical, moral, Political, Government, corruption, revolution"
-  //     },
-  //     {
-  //       "answer": "Occasionally",
-  //       "keywords": "Social justice, Civil rights, empowerment, Political, revolution"
-  //     },
-  //     {
-  //       "answer": "Not really"
-  //     },
-  //     {
-  //       "answer": "Never"
-  //     }
-  //   ]
-  // },
-  // {
-  //   "question": "Do you often find yourself making witty remarks or clever jokes in social situations?",
-  //   "answerOptions": [
-  //     {
-  //       "answer": "Yes",
-  //       "keywords": "comedy, humor, witty, funny, laughter, amusing, comic, laugh"
-  //     },
-  //     {
-  //       "answer": "Occasionally",
-  //       "keywords": "comedy, humor, funny"
-  //     },
-  //     {
-  //       "answer": "Not really"
-  //     },
-  //     {
-  //       "answer": "Never"
-  //     }
-  //   ]
-  // },
-  // {
-  //   "question": "How often do you find yourself drawn to the enchantment and wonder of magical realms, mythical creatures, and tales of ancient folklore?",
-  //   "answerOptions": [
-  //     {
-  //       "answer": "Yes",
-  //       "keywords": "Fantasy, Mythical Creatures, Magic, Enchantment, Quests, Potter, Imagination"
-  //     },
-  //     {
-  //       "answer": "Occasionally",
-  //       "keywords": "Fantasy, Magical, Imaginative"
-  //     },
-  //     {
-  //       "answer": "Not really"
-  //     },
-  //     {
-  //       "answer": "Never"
-  //     }
-  //   ]
-  // },
-  // {
-  //   "question": "Do you find yourself inspired by the heroism and extraordinary abilities of superheroes who fight for justice and protect others?",
-  //   "answerOptions": [
-  //     {
-  //       "answer": "Yes",
-  //       "keywords": "Action, Superhero, Heroism, Justice, Marvel, DC Comics, Avengers, X-Men, Batman, Spider-Man, Superman, Wonder Woman."
-  //     },
-  //     {
-  //       "answer": "Occasionally",
-  //       "keywords": "Superhero, Marvel, DC"
-  //     },
-  //     {
-  //       "answer": "Not really"
-  //     },
-  //     {
-  //       "answer": "Never"
-  //     }
-  //   ]
-  // },
-  // {
-  //   "question": "Do you enjoy following the latest fashion trends and experimenting with different styles?",
-  //   "answerOptions": [
-  //     {
-  //       "answer": "Yes",
-  //       "keywords": "glamor, costume, fashion, Style, Creative, Wardrobe, modeling, influencer, Transformation"
-  //     },
-  //     {
-  //       "answer": "Occasionally",
-  //       "keywords": "glamor, fashion, Style"
-  //     },
-  //     {
-  //       "answer": "Not really"
-  //     },
-  //     {
-  //       "answer": "Never"
-  //     }
-  //   ]
-  // },
-  //   {
-  //     "question": "Do you often seek opportunities for personal growth and self-improvement?",
-  //     "answerOptions": [
-  //     {
-  //     "answer": "Yes",
-  //     "keywords": "Motivational, Inspirational, Growth, Self-Improvement, Success, Achievement, Positive, Happiness"
-  //     },
-  //     {
-  //     "answer": "Occasionally",
-  //     "keywords": "Motivational, Inspirational, Success"
-  //     },
-  //     {
-  //     "answer": "Not really"
-  //     },
-  //     {
-  //     "answer": "Never"
-  //     }
-  //     ]
-  //     },
-  //     {
-  //     "question": "How often do you find yourself daydreaming about love stories or romantic scenarios?",
-  //     "answerOptions": [
-  //     {
-  //     "answer": "Yes",
-  //     "keywords": "Romantic Comedies, Love, Relationships, Passion, date, boyfriend, girlfriend"
-  //     },
-  //     {
-  //     "answer": "Occasionally",
-  //     "keywords": "Romantic Comedies, Feel-good, Joyful, Cheerful"
-  //     },
-  //     {
-  //     "answer": "Not really"
-  //     },
-  //     {
-  //     "answer": "Never"
-  //     }
-  //     ]
-  //     },
-  //     {
-  //     "question": "Do you enjoy attending live music performances and dancing?",
-  //     "answerOptions": [
-  //     {
-  //     "answer": "Yes",
-  //     "keywords": "Music, Musical, Dance, Performance, Rhythm, Choreography, Melody, Beat, Stage, Concert, Show"
-  //     },
-  //     {
-  //     "answer": "Occasionally",
-  //     "keywords": "Music, Dance, Choreography"
-  //     },
-  //     {
-  //     "answer": "Not really"
-  //     },
-  //     {
-  //     "answer": "Never"
-  //     }
-  //     ]
-  //     },
-  //     {
-  //     "question": "Do you actively participate in sports or athletic activities?",
-  //     "answerOptions": [
-  //     {
-  //     "answer": "Yes",
-  //     "keywords": "Active Sports, competitive games, team, athletics, competition, triumph, dedication"
-  //     },
-  //     {
-  //     "answer": "Occasionally",
-  //     "keywords": "Sports, games, athletics, competition"
-  //     },
-  //     {
-  //     "answer": "Not really"
-  //     },
-  //     {
-  //     "answer": "Never"
-  //     }
-  //     ]
-  //     }
+    {
+    "question": "Do you often find yourself reading or learning about historical events, figures, or civilizations?",
+    "answerOptions": [
+    {
+    "answer": "Yes",
+    "keywords": "Historical Epics, Period, Documentaries, War, Archaeological"
+    },
+    {
+    "answer": "Occasionally",
+    "keywords": "Historical Epics, Period"
+    },
+    {
+    "answer": "Not really"
+    },
+    {
+    "answer": "Never"
+    }
+    ]
+    },
+  {
+    "question": "Do you enjoy exploring the latest advancements in science and technology?",
+    "answerOptions": [
+      {
+        "answer": "Yes",
+        "keywords": "Science, Technology, Future, space, Robots, planet, universe, sci-fi"
+      },
+      {
+        "answer": "Occasionally",
+        "keywords": "Science, Technology, Robots, sci-fi"
+      },
+      {
+        "answer": "Not really"
+      },
+      {
+        "answer": "Never"
+      }
+    ]
+  },
+  {
+    "question": "Do you actively engage in discussions or activities for social causes and strive to make a positive impact in your community?",
+    "answerOptions": [
+      {
+        "answer": "Yes",
+        "keywords": "Social justice, Civil rights, discrimination, inequality, activism, empowerment, philosophical, moral, Political, Government, corruption, revolution"
+      },
+      {
+        "answer": "Occasionally",
+        "keywords": "Social justice, Civil rights, empowerment, Political, revolution"
+      },
+      {
+        "answer": "Not really"
+      },
+      {
+        "answer": "Never"
+      }
+    ]
+  },
+  {
+    "question": "Do you often find yourself making witty remarks or clever jokes in social situations?",
+    "answerOptions": [
+      {
+        "answer": "Yes",
+        "keywords": "comedy, humor, witty, funny, laughter, amusing, comic, laugh"
+      },
+      {
+        "answer": "Occasionally",
+        "keywords": "comedy, humor, funny"
+      },
+      {
+        "answer": "Not really"
+      },
+      {
+        "answer": "Never"
+      }
+    ]
+  },
+  {
+    "question": "How often do you find yourself drawn to the enchantment and wonder of magical realms, mythical creatures, and tales of ancient folklore?",
+    "answerOptions": [
+      {
+        "answer": "Yes",
+        "keywords": "Fantasy, Mythical Creatures, Magic, Enchantment, Quests, Potter, Imagination"
+      },
+      {
+        "answer": "Occasionally",
+        "keywords": "Fantasy, Magical, Imaginative"
+      },
+      {
+        "answer": "Not really"
+      },
+      {
+        "answer": "Never"
+      }
+    ]
+  },
+  {
+    "question": "Do you find yourself inspired by the heroism and extraordinary abilities of superheroes who fight for justice and protect others?",
+    "answerOptions": [
+      {
+        "answer": "Yes",
+        "keywords": "Action, Superhero, Heroism, Justice, Marvel, DC Comics, Avengers, X-Men, Batman, Spider-Man, Superman, Wonder Woman."
+      },
+      {
+        "answer": "Occasionally",
+        "keywords": "Superhero, Marvel, DC"
+      },
+      {
+        "answer": "Not really"
+      },
+      {
+        "answer": "Never"
+      }
+    ]
+  },
+  {
+    "question": "Do you enjoy following the latest fashion trends and experimenting with different styles?",
+    "answerOptions": [
+      {
+        "answer": "Yes",
+        "keywords": "glamor, costume, fashion, Style, Creative, Wardrobe, modeling, influencer, Transformation"
+      },
+      {
+        "answer": "Occasionally",
+        "keywords": "glamor, fashion, Style"
+      },
+      {
+        "answer": "Not really"
+      },
+      {
+        "answer": "Never"
+      }
+    ]
+  },
+    {
+      "question": "Do you often seek opportunities for personal growth and self-improvement?",
+      "answerOptions": [
+      {
+      "answer": "Yes",
+      "keywords": "Motivational, Inspirational, Growth, Self-Improvement, Success, Achievement, Positive, Happiness"
+      },
+      {
+      "answer": "Occasionally",
+      "keywords": "Motivational, Inspirational, Success"
+      },
+      {
+      "answer": "Not really"
+      },
+      {
+      "answer": "Never"
+      }
+      ]
+      },
+      {
+      "question": "How often do you find yourself daydreaming about love stories or romantic scenarios?",
+      "answerOptions": [
+      {
+      "answer": "Yes",
+      "keywords": "Romantic Comedies, Love, Relationships, Passion, date, boyfriend, girlfriend"
+      },
+      {
+      "answer": "Occasionally",
+      "keywords": "Romantic Comedies, Feel-good, Joyful, Cheerful"
+      },
+      {
+      "answer": "Not really"
+      },
+      {
+      "answer": "Never"
+      }
+      ]
+      },
+      {
+      "question": "Do you enjoy attending live music performances and dancing?",
+      "answerOptions": [
+      {
+      "answer": "Yes",
+      "keywords": "Music, Musical, Dance, Performance, Rhythm, Choreography, Melody, Beat, Stage, Concert, Show"
+      },
+      {
+      "answer": "Occasionally",
+      "keywords": "Music, Dance, Choreography"
+      },
+      {
+      "answer": "Not really"
+      },
+      {
+      "answer": "Never"
+      }
+      ]
+      },
+      {
+      "question": "Do you actively participate in sports or athletic activities?",
+      "answerOptions": [
+      {
+      "answer": "Yes",
+      "keywords": "Active Sports, competitive games, team, athletics, competition, triumph, dedication"
+      },
+      {
+      "answer": "Occasionally",
+      "keywords": "Sports, games, athletics, competition"
+      },
+      {
+      "answer": "Not really"
+      },
+      {
+      "answer": "Never"
+      }
+      ]
+      }
 ]
-
-
-
-
-
-
-// export const questions = [
-//   {
-//     "question": "When faced with a new challenge, I feel:",
-//     "answerOptions": [
-//       {
-//         "answer": "Excited and eager to explore new possibilities."
-//       },
-//       {
-//         "answer": "Anxious but willing to take calculated risks."
-//       },
-//       {
-//         "answer": "Intrigued and curious to unravel the unknown."
-//       },
-//       {
-//         "answer": "Playful and inclined to find humor in the situation."
-//       }
-//     ]
-//   },
-//   {
-//     "question": "My ideal way to spend free time is:",
-//     "answerOptions": [
-//       {
-//         "answer": "Engaging in artistic activities like painting, writing, or playing an instrument."
-//       },
-//       {
-//         "answer": "Seeking thrilling adventures or trying out extreme sports."
-//       },
-//       {
-//         "answer": "Exploring mysterious places or indulging in suspenseful novels and movies."
-//       },
-//       {
-//         "answer": "Socializing and making people laugh through jokes or storytelling."
-//       }
-//     ]
-//   },
-//   {
-//     "question": "When faced with a problem, I tend to:",
-//     "answerOptions": [
-//       {
-//         "answer": "Approach it from different angles and explore unconventional solutions."
-//       },
-//       {
-//         "answer": "Take immediate action and rely on my instincts."
-//       },
-//       {
-//         "answer": "Delve deep into research and gather information before making a decision."
-//       },
-//       {
-//         "answer": "Use humor and wit to diffuse tension and find a creative solution."
-//       }
-//     ]
-//   },
-  // {
-  //   "question": "In group settings, I am often seen as:",
-  //   "answerOptions": [
-  //     {
-  //       "answer": "The imaginative and artistic one."
-  //     },
-  //     {
-  //       "answer": "The adventurous and daring one."
-  //     },
-  //     {
-  //       "answer": "The enigmatic and mysterious one."
-  //     },
-  //     {
-  //       "answer": "The funny and entertaining one."
-  //     }
-  //   ]
-  // },
-  // {
-  //   "question": "When it comes to trying new things, I am:",
-  //   "answerOptions": [
-  //     {
-  //       "answer": "Open-minded and eager to explore uncharted territories."
-  //     },
-  //     {
-  //       "answer": "Willing to take risks and step out of my comfort zone."
-  //     },
-  //     {
-  //       "answer": "Intrigued by unconventional ideas and unconventional ways of thinking."
-  //     },
-  //     {
-  //       "answer": "Quick-witted and willing to experiment with humor and creativity."
-  //     }
-  //   ]
-  // },
-  // {
-  //   "question": "My friends would describe me as:",
-  //   "answerOptions": [
-  //     {
-  //       "answer": "Dreamy and romantic."
-  //     },
-  //     {
-  //       "answer": "Fearless and adventurous."
-  //     },
-  //     {
-  //       "answer": "Mysterious and intriguing."
-  //     },
-  //     {
-  //       "answer": "Funny and entertaining."
-  //     }
-  //   ]
-  // },
-  // {
-  //   "question": "I am most inspired by:",
-  //   "answerOptions": [
-  //     {
-  //       "answer": "Artistic expressions and creative works."
-  //     },
-  //     {
-  //       "answer": "Thrilling experiences and adrenaline-inducing activities."
-  //     },
-  //     {
-  //       "answer": "Enigmatic stories and complex concepts."
-  //     },
-  //     {
-  //       "answer": "Humor and comedic performances."
-  //     }
-  //   ]
-  // },
-  // {
-  //   "question": "When faced with a difficult decision, I rely on:",
-  //   "answerOptions": [
-  //     {
-  //       "answer": "My intuition and gut feelings."
-  //     },
-  //     {
-  //       "answer": "My willingness to take risks and embrace challenges."
-  //     },
-  //     {
-  //       "answer": "In-depth analysis and thoughtful consideration."
-  //     },
-  //     {
-  //       "answer": "My sense of humor and ability to find unconventional solutions."
-  //     }
-  //   ]
-  // }
-// ]
