800d19f4074a38086c982d5e6e03cc091f4cb601
Divya <dm.divya.mahajan@gmail.com>
2023-06-01 10:57:51 -0700

    mov

--- src/app/results/page.js
+++ src/app/results/page.js
@@ -1,31 +1,27 @@
-"use client";
 import Link from "next/link";
 import Movie from "./Movie.jsx";
 import { useState, useEffect } from "react";
 
 export default function Results(props) {
   const [movies, setMovies] = useState(null);
-  // console.log(props);
   const { searchQuery, personalityTitle, personalityDescription } =
     props.searchParams;
   const keywords = Object.keys(props.searchParams)[0];
-  const [ personalityData, setPersonalityData ] = useState(null);
+  const [personalityData, setPersonalityData] = useState(null);
 
   useEffect(() => {
-    // console.log(searchQuery);
-    // console.log(personalityTitle);
-    // console.log(personalityDescription);
-    // console.log(Object.keys(props.searchParams)[0]);
-    getPersonalityType(keywords);
-    getMovieRecommendations(keywords);
+    getMovieRecommendations(keywords); // Call getMovieRecommendations directly
     return () => {};
   }, []);
 
-  async function getPersonalityType(keywords) {
-    let personalityData = await fetch('/api/personality' , {
-      method: 'POST',
-      body: JSON.stringify({ keywords : keywords})
-    })
+  async function getPersonalityType(filteredMovies) {
+    let personalityData = await fetch("/api/personality", {
+      method: "POST",
+      body: JSON.stringify({
+        keywords: Object.keys(props.searchParams)[0],
+        filteredMovies,
+      }),
+    });
     personalityData = await personalityData.json();
     personalityData = await JSON.parse(personalityData.data);
     setPersonalityData(personalityData);
@@ -35,14 +31,14 @@ export default function Results(props) {
     try {
       const response = await fetch("/api/recommendationAPI", {
         method: "POST",
-        body: {query},
+        body: JSON.stringify({ query }),
       });
       const responseData = await response.json();
 
       let filteredMovies = [];
       let i = 0;
       const englishRegexp = /^[a-zA-Z0-9\s]+$/;
-      while (filteredMovies.length != 5) {
+      while (filteredMovies.length !== 5) {
         if (
           responseData[i].title &&
           englishRegexp.test(responseData[i].title) &&
@@ -54,22 +50,23 @@ export default function Results(props) {
       }
 
       setMovies(filteredMovies);
+      getPersonalityType(filteredMovies); // Call getPersonalityType with filteredMovies
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   }
 
   if (!movies) {
-    return;
+    return null; // Return null instead of nothing
   }
-  // movieRecommendations.forEach((movie) => {
-  //   console.log(movie.movieId);
-  // });
+
   return (
     <main className="bg-gray-900">
       <div className="text-white text-xl p-5 w-3/4">
         <h2 className="text-4xl font-bold mb-4">
-          {personalityData ? personalityData?.title : "Loading personality title"}
+          {personalityData
+            ? personalityData?.title
+            : "Loading personality title"}
         </h2>
         <p>
           {personalityData
