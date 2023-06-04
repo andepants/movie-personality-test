9182709a6f0a0a2272c36a68cfe16c269929cd82
Andrew Heim <andrewsheim@gmail.com>
2023-06-04 16:24:45 -0600

    removing filter on movies

--- src/app/results/page.js
+++ src/app/results/page.js
@@ -33,10 +33,10 @@ export default function Results(props) {
         const englishRegexp = /^[a-zA-Z0-9\s]+$/;
         while (filteredMovies.length != 10) {
           if (
-            // true
-            responseData[i]?.title &&
-            englishRegexp.test(responseData[i]?.title) &&
-            responseData[i]?.overview.trim()
+            true
+            // responseData[i]?.title &&
+            // englishRegexp.test(responseData[i]?.title) &&
+            // responseData[i]?.overview.trim()
           ) {
             filteredMovies.push(responseData[i]);
           }
