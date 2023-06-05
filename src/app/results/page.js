a99ebd1425a8e0bf1348e269972e5e94a411bb90
Andrew Heim <andrewsheim@gmail.com>
2023-06-04 18:33:20 -0600

    adding console.logs

--- src/app/results/page.js
+++ src/app/results/page.js
@@ -20,6 +20,7 @@ export default function Results(props) {
   }
 
   async function getMovieRecommendations(query) {
+    console.log('query', Object.keys(props.searchParams)[0]);
     try {
       const response = await fetch("/api/recommendationAPI", {
         method: "POST",
@@ -29,6 +30,7 @@ export default function Results(props) {
         },
       });
       const responseData = await response.json();
+      console.log('responseData', responseData);
       let filteredMovies = [];
       let i = 0;
       const englishRegexp = /^[a-zA-Z0-9\s]+$/;
@@ -43,6 +45,7 @@ export default function Results(props) {
         }
         i++;
       }
+      console.log('filteredMovies', filteredMovies)
       setMovies(filteredMovies);
       let movieTitles = '';
       for (let i = 0; i < filteredMovies.length; i++) {
